a3a4e516b7367f63d01037d67cfaa8b3f21bb589
bigwave-cho <hooyaa93@gmail.com>
2023-03-20 09:08:28 +0900

    initial settings : install tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+/*
+## Next.js에서 tailwind 세팅하기
+https://tailwindcss.com/docs/guides/nextjs
+*/
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
